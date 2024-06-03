73593c985b01f06c7cdb4e2b9ddfb4a686fbd989
Sayed Ali Alawi <75791634+oldhunterr@users.noreply.github.com>
2024-06-03 12:44:59 +0300

    Update radarr.sh

--- radarr.sh
+++ radarr.sh
@@ -19,9 +19,7 @@ cat <<"EOF"
   / /_/ / __ `/ __  / __ `/ ___/ ___/
  / _, _/ /_/ / /_/ / /_/ / /  / /    
 /_/ |_|\__,_/\__,_/\__,_/_/  /_/     
-@@@@@@
-   V2
-@@@@@
+    V3
 EOF
 }
 header_info
@@ -60,41 +58,41 @@ function default_settings() {
   echo_default
 }
 
-function install_sonarr() {
-  msg_info "Installing Sonarr"
-  wget -q -O - https://apt.sonarr.tv/pub.key | apt-key add -
-  echo "deb https://apt.sonarr.tv/debian buster main" | tee /etc/apt/sources.list.d/sonarr.list
-  apt update
-  apt install nzbdrone -y
-  systemctl enable sonarr
-  systemctl start sonarr
-  msg_ok "Sonarr Installed"
-}
+# function install_sonarr() {
+#   msg_info "Installing Sonarr"
+#   wget -q -O - https://apt.sonarr.tv/pub.key | sudo apt-key add -
+#   echo "deb https://apt.sonarr.tv/debian buster main" | sudo tee /etc/apt/sources.list.d/sonarr.list
+#   sudo apt update
+#   sudo apt install nzbdrone -y
+#   sudo systemctl enable sonarr
+#   sudo systemctl start sonarr
+#   msg_ok "Sonarr Installed"
+# }
 
-function install_radarr() {
-  msg_info "Installing Radarr"
-  wget -q -O - https://apt.sonarr.tv/pub.key | apt-key add -
-  echo "deb https://apt.sonarr.tv/ubuntu bionic main" | tee /etc/apt/sources.list.d/radarr.list
-  apt update
-  apt install radarr -y
-  systemctl enable radarr
-  systemctl start radarr
-  msg_ok "Radarr Installed"
-}
+# function install_radarr() {
+#   msg_info "Installing Radarr"
+#   wget -q -O - https://apt.sonarr.tv/pub.key | sudo apt-key add -
+#   echo "deb https://apt.sonarr.tv/ubuntu bionic main" | sudo tee /etc/apt/sources.list.d/radarr.list
+#   sudo apt update
+#   sudo apt install radarr -y
+#   sudo systemctl enable radarr
+#   sudo systemctl start radarr
+#   msg_ok "Radarr Installed"
+# }
 
 function update_sonarr() {
-  header_info
-  if [[ ! -d /opt/Sonarr ]]; then msg_error "No Sonarr Installation Found!"; exit; fi
-  msg_info "Updating Sonarr v4"
-  systemctl stop sonarr.service
-  wget -q -O SonarrV4.tar.gz 'https://services.sonarr.tv/v1/download/main/latest?version=4&os=linux&arch=x64'
-  tar -xzf SonarrV4.tar.gz
-  rm -rf /opt/Sonarr
-  mv Sonarr /opt
-  rm -rf SonarrV4.tar.gz
-  systemctl start sonarr.service
-  msg_ok "Updated Sonarr v4"
-  exit
+header_info
+if [[ ! -d /opt/Sonarr ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+msg_info "Updating $APP v4"
+systemctl stop sonarr.service
+wget -q -O SonarrV4.tar.gz 'https://services.sonarr.tv/v1/download/main/latest?version=4&os=linux&arch=x64'
+tar -xzf SonarrV4.tar.gz
+rm -rf /opt/Sonarr
+mv Sonarr /opt
+rm -rf SonarrV4.tar.gz
+systemctl start sonarr.service
+msg_ok "Updated $APP v4"
+exit
 }
 
 function update_radarr() {
@@ -112,8 +110,8 @@ build_container
 description
 
 msg_info "Starting Installation of Sonarr and Radarr"
-install_sonarr
-install_radarr
+update_sonarr
+update_radarr
 
 msg_ok "Completed Successfully!\n"
 echo -e "Sonarr should be reachable by going to the following URL:
